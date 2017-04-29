85bc0f5c66f43fb5eb330f33e2f5659e9d7fd9ec
Tony Brown <tony@tonybrown.me>
2017-04-28 21:59:30 -0400

    initial commit, demo content

---
+++ server.js
@@ -0,0 +1,41 @@
+
+
+
+//import http from 'http';
+
+//const server = http.createServer((req, res) => {
+  //res.write('Hello HTTP node module, you are so awesome!\n');
+  //setTimeout(() => {
+    //res.write('[After 3 seconds]...I can stream too!\n');
+    //res.end();
+  //}, 3000);
+//});
+
+//server.listen(8080);
+
+//server.on('request', (req, res) => {
+  //res.write('Hello HTTP node module, you are so awesome!\n');
+  //setTimeout(() => {
+    //res.write('[after 3 seconds]...I can stream too!\n');
+    //res.end();
+  //}, 3000);
+//});
+
+
+//import https from 'https';
+
+//https.get('https://www.lynda.com', res => {
+  //console.log('Response status code: ', res.statusCode);
+
+  //res.on('data', chunk => {
+    //console.log(chunk.toString());
+  //});
+//});
+
+
+
+//import config, { nodeEnv, logStars } from './config'
+
+//console.log(config, nodeEnv,);
+//logStars('My message')
+
