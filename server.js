cb76239d5d50fef6977edd18d091c728fe66111e
Tony Brown <tony@tonybrown.me>
2017-05-10 16:14:21 -0400

    finish setting up EJS and routing

--- server.js
+++ server.js
@@ -1,62 +1,20 @@
 import config from './config';
-// import fs from 'fs';
 import apiRouter from './api';
 import express from 'express';
 
 const server = express();
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-  res.send('Hello Express\n');
+  res.render('index', {
+    content:'<h4>Hello Express and <em><b>EJS</b></em>!!!</h4>'
+  });
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-// server.get('/about.html', (req, res) => {
-//   fs.readFile('./about.html', (err, data) => {
-//     res.send(data.toString());
-//   });
-// });
-
 server.listen(config.port, () => {
   console.info('Express is listening on port ', config.port);
 });
-
-// import config, { nodeEnv, logStars } from './config';
-//
-// console.info(config, nodeEnv,);
-// logStars('My message');
-
-
-
-// import http from 'http';
-//
-// const server = http.createServer((req, res) => {
-//   res.write('Hello HTTP node module, you are so awesome!\n');
-//   setTimeout(() => {
-//     res.write('[After 3 seconds]...I can stream too!\n');
-//     res.end();
-//   }, 3000);
-// });
-//
-//
-// server.listen(8080);
-
-// server.on('request', (req, res) => {
-//   res.write('Hello HTTP node module, you are so awesome!\n');
-//   setTimeout(() => {
-//     res.write('[after 3 seconds]...I can stream too!\n');
-//     res.end();
-//   }, 3000);
-// });
-
-
-// import https from 'https';
-//
-// https.get('https://www.lynda.com', res => {
-//   console.log('Response status code: ', res.statusCode);
-//
-//   res.on('data', chunk => {
-//     console.log(chunk.toString());
-//   });
-// });
