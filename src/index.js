a37c42ea4a63896b03045492da1f6cc1b6659618
Tony Brown <tony@tonybrown.me>
2017-05-10 21:00:34 -0400

    make modular app with a clear dependency tree

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-const Header = ({message}) => {
-  return (
-    <h2 className='Header text-center text-muted'>
-      {message}
-    </h2>
-  );
-};
-
-Header.propTypes = {
-  message: PropTypes.string.isRequired
-};
-
-const App = () => {
-  return (
-    <div className='App'>
-      <Header message='Naming Contest' />
-    </div>
-  );
-};
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
