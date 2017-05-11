44b7da23fb455532b9fc8a96318afb0c1a908a34
Tony Brown <tony@tonybrown.me>
2017-05-10 21:30:25 -0400

    add state to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div className='App'>
-      <Header message='Naming Contest' />
-    </div>
-  );
-};
+class App extends Component {
+// Since we are using babel-preset-stage-2
+// we can use a shorter syntax
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = { test: 42 };
+  // }
 
+  render() {
+    return (
+      <div className='App'>
+        <Header message={this.state.pageHeader} />
+        <div>
+          {this.state.test}
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
