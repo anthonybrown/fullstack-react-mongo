3caa76aa6942809f8f0e826d25fc1a97e52db414
Tony Brown <tony@tonybrown.me>
2017-05-11 14:00:37 -0400

    data is being served from the backend if JavaScript is disabled

---
+++ serverRender.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import config from './config';
+import App from './src/components/App';
+
+const serverRender = () =>
+  axios.get(`${config.serverUrl}/api/contests`)
+    .then(response => {
+      return ReactDOMServer.renderToString(
+        <App initialContests={response.data.contests} />
+      );
+    }).catch(console.error);
+
+export default serverRender;
